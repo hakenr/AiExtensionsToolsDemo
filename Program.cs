48f472d5be000b47284c2e655aaba6176d03f95d
Robert Haken <haken@havit.cz>
2026-03-04 14:34:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using Microsoft.Extensions.AI;
+
+IChatClient client =
+	new Azure.AI.OpenAI.AzureOpenAIClient(
+			new Uri("https://hakenoai.openai.azure.com/"),
+			new System.ClientModel.ApiKeyCredential("****API-KEY****"))
+		.GetChatClient("gpt-4o")
+		.AsIChatClient()
+
+		.AsBuilder()
+		.UseFunctionInvocation()
+		.Build();
+
+var options = new ChatOptions()
+{
+	Tools = [
+		AIFunctionFactory.Create(GetCurrentTime),
+		//AIFunctionFactory.Create(Multiply)
+	],
+};
+
+var response = await client.GetResponseAsync("What time is it?", options);
+//var response = await client.GetResponseAsync("How much is 9756 multiplied by 7136?", options);
+
+Console.WriteLine(response.Text);
+
+
+[Description("Gets the current date and time.")]
+static string GetCurrentTime()
+{
+	return DateTime.Now.ToString();
+}
+
+[Description("Multiplies two integers and returns the result.")]
+static int Multiply([Description("The first integer.")] int a, [Description("The second integer.")] int b)
+{
+	return a * b;
+}
